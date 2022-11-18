ed3ad38fa5e94a78005f4e1fc522d3e1f0b7b373
khelendrabithle <118499886+khelendrabithle@users.noreply.github.com>
2022-11-18 10:59:09 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,16 @@
+public class Time {
+    public static void main(String[] args) {
+
+
+
+            int hor = 21;
+            int min = 58;
+            int sec = 55;
+
+            for (int i = 0; i <= hor; i++) ;
+            {
+                System.out.println(hor+":"+ hor+":"+ hor);
+            }
+
+    }
+}
